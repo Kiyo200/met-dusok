b5e62508c3973d6c929f585f9a56d51aaf058ab1
Kiyo200 <darknessblack2007@gmail.com>
2024-11-05 09:31:51 +0100

    1-2 + metodussal tobb

--- metódusok/Program.cs
+++ metódusok/Program.cs
@@ -6,6 +6,11 @@
 		{
 			Koszonto();
             Console.WriteLine(Osszeadas(10,90));
+            Console.WriteLine(Duplaz(5));
+            Console.WriteLine(Osszeg(4));
+			int elso = 5;
+			int masodik = 4;
+            Console.WriteLine(elso+ " " +masodik);
         }
 		static void Koszonto()
 		{
@@ -22,6 +27,23 @@
 			/*return a + b;*/
 		}
 
+		static int Duplaz( int szam)
+		{
+			return szam * 2;
+		}
 
+		static int Osszeg(int a, int b = 5)
+		{
+			return a + b;
+		}
+
+
+		static void Csere(ref int a,ref int b)
+		{
+			int tep = a;
+			a = b;
+			b = tep;
+
+		}
 	}
 }

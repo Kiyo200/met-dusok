3fbac778f10c8c137b134de6939250a65386675c
Kiyo200 <darknessblack2007@gmail.com>
2024-11-05 09:02:54 +0100

    kezdeti metodusok atnezese

--- metódusok/Program.cs
+++ metódusok/Program.cs
@@ -4,7 +4,24 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			Koszonto();
+            Console.WriteLine(Osszeadas(10,90));
+        }
+		static void Koszonto()
+		{
+            Console.WriteLine("hello vilag");
+			Console.ReadLine();
+        }
+
+
+
+		static int Osszeadas(int a, int b)
+		{
+			int osszeg = a + b;
+			return osszeg;
+			/*return a + b;*/
 		}
+
+
 	}
 }
